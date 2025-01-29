b0e0e37adf8d06d8e5dadac2fd260e0e34640f08
Alejandro Novellino <alnovellino07@gmail.com>
2025-01-29 18:32:46 -0400

    Adds a24-ai-api project

---
+++ apps/a24-ai/a24-ai-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'a24-ai-api',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/a24-ai/a24-ai-api',
+};
